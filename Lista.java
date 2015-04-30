d17dc6f27a0898dab680841a18bfae7401b400b4
Oscar Sanchez <osanchez@MAC-SOFT7.local>
2015-04-30 11:25:05 -0500

    Ejercicio numero 3

---
+++ Lista.java
@@ -0,0 +1,37 @@
+
+package UPT.JAVA.PRINCIPAL;
+
+
+public class Lista {
+   
+    private String codigo;
+    private String nombre;
+    private String apellidos;
+    private String edad;
+    
+    public String getCodigo(){
+        return codigo;
+    }
+    public void setCodigo(String codigo){
+        this.codigo = codigo;
+    }
+    public String getNombre(){
+        return nombre;
+    }
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+    public String getApellidos(){
+        return apellidos;
+    }
+    public void setApellidos(String apellidos){
+        this.apellidos = apellidos;
+    }
+    public String getEdad(){
+        return edad;
+    }
+    public void setEdad(String edad){
+        this.edad = edad;
+    }
+    
+}
